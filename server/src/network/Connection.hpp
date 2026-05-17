3c0e7c0ccb617451303f136c6bf0619037ecbaa9
Gleb Melnik <339891@niuitmo.ru>
2026-05-17 17:02:41 +0300

    Rework connection message sending to be able to send multiple messages in TCP-parallel

--- server/src/network/Connection.hpp
+++ server/src/network/Connection.hpp
@@ -51,6 +51,7 @@ private:
     std::string remoteEndpointString() const noexcept;
 
 private:
+    ConnectionWriter m_writer;
     asio::ip::tcp::socket m_socket;
     ConnectionWriter m_writer;
     MessageChannel m_messageChannel;

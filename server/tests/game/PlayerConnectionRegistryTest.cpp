859c6eae5771c856c144585715a374e39756f704
Gleb Melnik <339891@niuitmo.ru>
2026-06-26 13:20:56 +0300

    Refactor `PlayerConnectionRegistryTest`: adopt `player_connection_registry_fixture` for better test setup reuse and replace inline constants with unified test helpers.

--- server/tests/game/PlayerConnectionRegistryTest.cpp
+++ server/tests/game/PlayerConnectionRegistryTest.cpp
@@ -1,179 +1,184 @@
 #include "server/src/game/PlayerConnectionRegistry.hpp"
-
-#include <optional>
+#include "utils/game_fixtures.hpp"
+#include "utils/game_test_constants.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
 namespace s2d::game
 {
-namespace
+using s2d::test::game::connection_1;
+using s2d::test::game::connection_2;
+using s2d::test::game::connection_3;
+using s2d::test::game::player_1;
+using s2d::test::game::player_2;
+using s2d::test::game::player_3;
+
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::playerFor returns nullopt for unknown connection",
+    "[PlayerConnectionRegistry]")
 {
-
-constexpr connection_key connection1{1};
-constexpr connection_key connection2{2};
-constexpr connection_key connection3{3};
-
-constexpr player_id player1{10};
-constexpr player_id player2{20};
-constexpr player_id player3{30};
-
-} // namespace
-
-TEST_CASE("PlayerConnectionRegistry::playerFor returns nullopt for unknown connection", "[PlayerConnectionRegistry]")
-{
-    PlayerConnectionRegistry registry;
-
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
-    REQUIRE_FALSE(registry.playerFor(connection2).has_value());
-    REQUIRE_FALSE(registry.playerFor(connection3).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_2).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_3).has_value());
 }
 
-TEST_CASE("PlayerConnectionRegistry::bind binds connection to player", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::bind binds connection to player",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
 
-    registry.bind(connection1, player1);
-
-    auto const player = registry.playerFor(connection1);
+    auto const player = registry.playerFor(connection_1);
 
     REQUIRE(player.has_value());
-    REQUIRE(player.value() == player1);
+    REQUIRE(player.value() == player_1);
 }
 
-TEST_CASE("PlayerConnectionRegistry::bind supports multiple independent connections", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::bind supports multiple independent connections",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
+    registry.bind(connection_2, player_2);
+    registry.bind(connection_3, player_3);
 
-    registry.bind(connection1, player1);
-    registry.bind(connection2, player2);
-    registry.bind(connection3, player3);
+    REQUIRE(registry.playerFor(connection_1).has_value());
+    REQUIRE(registry.playerFor(connection_2).has_value());
+    REQUIRE(registry.playerFor(connection_3).has_value());
 
-    REQUIRE(registry.playerFor(connection1).has_value());
-    REQUIRE(registry.playerFor(connection2).has_value());
-    REQUIRE(registry.playerFor(connection3).has_value());
-
-    REQUIRE(registry.playerFor(connection1).value() == player1);
-    REQUIRE(registry.playerFor(connection2).value() == player2);
-    REQUIRE(registry.playerFor(connection3).value() == player3);
+    REQUIRE(registry.playerFor(connection_1).value() == player_1);
+    REQUIRE(registry.playerFor(connection_2).value() == player_2);
+    REQUIRE(registry.playerFor(connection_3).value() == player_3);
 }
 
-TEST_CASE("PlayerConnectionRegistry::bind overwrites existing binding for same connection", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::bind overwrites existing binding for same connection",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
 
-    registry.bind(connection1, player1);
+    REQUIRE(registry.playerFor(connection_1).has_value());
+    REQUIRE(registry.playerFor(connection_1).value() == player_1);
 
-    REQUIRE(registry.playerFor(connection1).has_value());
-    REQUIRE(registry.playerFor(connection1).value() == player1);
+    registry.bind(connection_1, player_2);
 
-    registry.bind(connection1, player2);
-
-    auto const player = registry.playerFor(connection1);
+    auto const player = registry.playerFor(connection_1);
 
     REQUIRE(player.has_value());
-    REQUIRE(player.value() == player2);
+    REQUIRE(player.value() == player_2);
 }
 
-TEST_CASE("PlayerConnectionRegistry::bind overwrite does not affect other connections", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::bind overwrite does not affect other connections",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
+    registry.bind(connection_2, player_2);
 
-    registry.bind(connection1, player1);
-    registry.bind(connection2, player2);
+    registry.bind(connection_1, player_3);
 
-    registry.bind(connection1, player3);
+    REQUIRE(registry.playerFor(connection_1).has_value());
+    REQUIRE(registry.playerFor(connection_2).has_value());
 
-    REQUIRE(registry.playerFor(connection1).has_value());
-    REQUIRE(registry.playerFor(connection2).has_value());
-
-    REQUIRE(registry.playerFor(connection1).value() == player3);
-    REQUIRE(registry.playerFor(connection2).value() == player2);
+    REQUIRE(registry.playerFor(connection_1).value() == player_3);
+    REQUIRE(registry.playerFor(connection_2).value() == player_2);
 }
 
-TEST_CASE("PlayerConnectionRegistry::unbind removes existing binding", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::unbind removes existing binding",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
 
-    registry.bind(connection1, player1);
+    REQUIRE(registry.playerFor(connection_1).has_value());
 
-    REQUIRE(registry.playerFor(connection1).has_value());
+    registry.unbind(connection_1);
 
-    registry.unbind(connection1);
-
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
 }
 
-TEST_CASE("PlayerConnectionRegistry::unbind unknown connection is no-op", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::unbind unknown connection is no-op",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.unbind(connection_1);
 
-    registry.unbind(connection1);
-
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
 }
 
-TEST_CASE("PlayerConnectionRegistry::unbind unknown connection does not affect existing bindings", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::unbind unknown connection does not affect existing bindings",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
+    registry.bind(connection_2, player_2);
 
-    registry.bind(connection1, player1);
-    registry.bind(connection2, player2);
+    registry.unbind(connection_3);
 
-    registry.unbind(connection3);
+    REQUIRE(registry.playerFor(connection_1).has_value());
+    REQUIRE(registry.playerFor(connection_2).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_3).has_value());
 
-    REQUIRE(registry.playerFor(connection1).has_value());
-    REQUIRE(registry.playerFor(connection2).has_value());
-    REQUIRE_FALSE(registry.playerFor(connection3).has_value());
-
-    REQUIRE(registry.playerFor(connection1).value() == player1);
-    REQUIRE(registry.playerFor(connection2).value() == player2);
+    REQUIRE(registry.playerFor(connection_1).value() == player_1);
+    REQUIRE(registry.playerFor(connection_2).value() == player_2);
 }
 
-TEST_CASE("PlayerConnectionRegistry::unbind removes only requested connection", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::unbind removes only requested connection",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
+    registry.bind(connection_2, player_2);
 
-    registry.bind(connection1, player1);
-    registry.bind(connection2, player2);
+    registry.unbind(connection_1);
 
-    registry.unbind(connection1);
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
 
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
-
-    auto const player = registry.playerFor(connection2);
+    auto const player = registry.playerFor(connection_2);
 
     REQUIRE(player.has_value());
-    REQUIRE(player.value() == player2);
+    REQUIRE(player.value() == player_2);
 }
 
-TEST_CASE("PlayerConnectionRegistry::unbind can be called repeatedly", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry::unbind can be called repeatedly",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
 
-    registry.bind(connection1, player1);
+    registry.unbind(connection_1);
+    registry.unbind(connection_1);
+    registry.unbind(connection_1);
 
-    registry.unbind(connection1);
-    registry.unbind(connection1);
-    registry.unbind(connection1);
-
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
 }
 
-TEST_CASE("PlayerConnectionRegistry can bind connection again after unbind", "[PlayerConnectionRegistry]")
+TEST_CASE_METHOD(
+    s2d::test::game::player_connection_registry_fixture,
+    "PlayerConnectionRegistry can bind connection again after unbind",
+    "[PlayerConnectionRegistry]")
 {
-    PlayerConnectionRegistry registry;
+    registry.bind(connection_1, player_1);
+    registry.unbind(connection_1);
 
-    registry.bind(connection1, player1);
-    registry.unbind(connection1);
+    REQUIRE_FALSE(registry.playerFor(connection_1).has_value());
 
-    REQUIRE_FALSE(registry.playerFor(connection1).has_value());
+    registry.bind(connection_1, player_2);
 
-    registry.bind(connection1, player2);
-
-    auto const player = registry.playerFor(connection1);
+    auto const player = registry.playerFor(connection_1);
 
     REQUIRE(player.has_value());
-    REQUIRE(player.value() == player2);
+    REQUIRE(player.value() == player_2);
 }
 
 } // namespace s2d::game

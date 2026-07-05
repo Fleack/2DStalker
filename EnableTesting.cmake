98c77311ff2706d289d0aacca75ae9a354f81366
Gleb Melnik <339891@niuitmo.ru>
2026-07-05 16:32:11 +0300

    Convert `SharedProtocol` to a shared library and adjust dependencies and configurations across the project.

--- EnableTesting.cmake
+++ EnableTesting.cmake
@@ -17,10 +17,12 @@ function(s2d_add_test target_name)
     )
 
     add_executable(${target_name}
-            ${ARG_SOURCES})
+            ${ARG_SOURCES}
+    )
 
     target_include_directories(${target_name} PRIVATE
-            ${PROJECT_SOURCE_DIR})
+            ${PROJECT_SOURCE_DIR}
+    )
 
     target_link_libraries(${target_name} PRIVATE
             Catch2::Catch2WithMain
@@ -28,11 +30,29 @@ function(s2d_add_test target_name)
             ${ARG_LIBS}
     )
 
-    add_test(NAME ${target_name} COMMAND ${target_name})
+    add_test(NAME ${target_name}
+            COMMAND ${target_name}
+    )
+
+    if (WIN32)
+        add_custom_command(TARGET ${target_name} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy -t
+                $<TARGET_FILE_DIR:${target_name}>
+                $<TARGET_RUNTIME_DLLS:${target_name}>
+                COMMAND_EXPAND_LISTS
+                VERBATIM
+        )
+    endif ()
 
     if (MINGW)
-        get_filename_component(S2D_CXX_RUNTIME_DIR "${CMAKE_CXX_COMPILER}" DIRECTORY)
+        get_filename_component(S2D_CXX_RUNTIME_DIR
+                "${CMAKE_CXX_COMPILER}"
+                DIRECTORY
+        )
+
         set_tests_properties(${target_name} PROPERTIES
-                ENVIRONMENT_MODIFICATION "PATH=path_list_prepend:${S2D_CXX_RUNTIME_DIR}")
+                ENVIRONMENT_MODIFICATION
+                "PATH=path_list_prepend:${S2D_CXX_RUNTIME_DIR}"
+        )
     endif ()
 endfunction()

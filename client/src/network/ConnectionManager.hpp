06214d37830cf78274c500adb1dde20bc436fcf5
Gleb Melnik <339891@niuitmo.ru>
2026-07-24 15:55:42 +0300

    Add `ConnectionManager` implementation to manage network client connection lifecycle

---
+++ client/src/network/ConnectionManager.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "ClientRequestManager.hpp"
+#include "ClientTypes.hpp"
+
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <optional>
+
+#include <boost/asio/any_io_executor.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/experimental/channel.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+
+namespace network
+{
+class ConnectionManager : public std::enable_shared_from_this<ConnectionManager>
+{
+public:
+    ConnectionManager(
+        asio::strand<asio::any_io_executor> strand,
+        Config const& config,
+        std::shared_ptr<ClientRequestManager> manager);
+
+    ConnectionManager(ConnectionManager const&) = delete;
+    ConnectionManager& operator=(ConnectionManager const&) = delete;
+    ConnectionManager(ConnectionManager&&) = delete;
+    ConnectionManager& operator=(ConnectionManager&&) = delete;
+
+    asio::awaitable<void> connect(asio::ip::tcp::endpoint endpoint);
+    asio::awaitable<void> disconnect();
+
+    [[nodiscard]] ConnectionState state() const noexcept;
+
+private:
+    void closeConnectingSocket() noexcept;
+    void connectionClosed();
+    void setState(ConnectionState state);
+
+private:
+    asio::strand<asio::any_io_executor> m_strand;
+    std::chrono::steady_clock::duration m_connectTimeout;
+    std::shared_ptr<ClientRequestManager> m_manager;
+
+    std::atomic<ConnectionState> m_state{ConnectionState::Disconnected};
+    std::optional<asio::ip::tcp::socket> m_connectingSocket;
+    asio::experimental::channel<void(system::error_code)> m_disconnectCompleted;
+};
+} // namespace network
